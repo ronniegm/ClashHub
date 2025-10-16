cc815da9211581d218dcf4e43782f9dc944bbe93
ronniegm <66147384+ronniegm@users.noreply.github.com>
2025-10-16 15:38:21 -0600

    Update Usuario.h

--- include/Usuario.h
+++ include/Usuario.h
@@ -1,52 +1,59 @@
 #ifndef USUARIO_H
 #define USUARIO_H
 
-
-// General Dependencies
-
+#include <string>
+#include <iostream>
 #include <vector>
 #include <map>
+#include <string>
+#include <locale>
+#include <codecvt>
 
-// * Class Required
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 
 #include "Referencia_de_Equipo.h"
+using namespace std;
 
 class Usuario
 {
-    public:
+public:
 
-        Usuario();
-        Usuario(string gamertag, string email, string password);
+    Usuario();
+    Usuario(string gamertag, string email, string password);
 
-        // static bool validate_User_Existance_By_Email();
-        bool authenticate_Login_Crendentials(string user_Password);
-        static bool update_User_Gamertag(string new_Gamertag);
-        static bool update_User_ELO(int new_ELO);
-        static bool add_User_Team_Reference(string Referencia_de_Equipo);
+ 
+    bool authenticate_Login_Crendentials(string user_Password);
+    void update_User_Gamertag(string new_Gamertag);
+    void update_User_ELO(int new_ELO);
+    void add_User_Team_Reference(Referencia_de_Equipo* equipo);
+    string get_Email();
 
-        string get_Email();
+   
+    void show_User_Info() const;
 
-        // * Database Methods
+    
+    string serialize() const;
+    static Usuario deserialize(const string& line);
 
-        string serialize() const;
-        static Usuario deserialize(const string& line);
-        map<string,string> toMap() const {
-            map<string,string> m;
-            m["gamertag"] = gamertag;
-            m["email"] = email;
-            m["password"] = password;
-            m["ELO"] = to_string(ELO);
-            return m;
-        }
+   
+    map<string, string> toMap() const {
+        map<string, string> m;
+        m["gamertag"] = gamertag;
+        m["email"] = email;
+        m["password"] = password;
+        m["ELO"] = to_string(ELO);
+        return m;
+    }
 
-    private:
-
-        string gamertag;
-        string email;
-        string password;
-        int ELO;
-        vector<Referencia_de_Equipo*> teams;
-
+private:
+    string gamertag;
+    string email;
+    string password;
+    int ELO;
+    vector<Referencia_de_Equipo*> teams;
 };
 
 #endif // USUARIO_H
